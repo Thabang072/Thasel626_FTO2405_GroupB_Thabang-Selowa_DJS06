d88b8dd2380a6a6e7956bbc6c95c82b919924507
Thabang Selowa <mohale20cobra@gmail.com>
2024-09-20 17:28:18 +0200

    Add forEach method to log each name and each province to the console.

--- scripts.js
+++ scripts.js
@@ -1,15 +1,20 @@
-// A list of provinces:
-const provinces = ['Western Cape', 'Gauteng', 'Northern Cape', 'Eastern Cape', 'KwaZulu-Natal', 'Free State']
-
-// A list of names:
-const names = ['Ashwin', 'Sibongile', 'Jan-Hendrik', 'Sifso', 'Shailen', 'Frikkie']
-
-// A list of products with prices:
+// Data Sets
+const provinces = ['Western Cape', 'Gauteng', 'Northern Cape', 'Eastern Cape', 'KwaZulu-Natal', 'Free State'];
+const names = ['Ashwin', 'Sibongile', 'Jan-Hendrik', 'Sifso', 'Shailen', 'Frikkie'];
 const products = [
   { product: 'banana', price: "2" },
   { product: 'mango', price: 6 },
-  { product: 'potato', price: ' ' },
+  { product: 'potato', price: ' ' }, // Empty price
   { product: 'avocado', price: "8" },
   { product: 'coffee', price: 10 },
-  { product: 'tea', price: '' },
-]
+  { product: 'tea', price: '' }, // Empty price
+];
+
+// 1. ForEach Basics
+// Log each province to the console
+provinces.forEach(province => console.log(province));
+// Log each name to the console
+names.forEach(name => console.log(name));
+// Log each name along with its corresponding province
+names.forEach((name, index) => console.log(`${name} (${provinces[index]})`));
+
